6cb9d8c6b01c3f6afb0d1184e35635e1499dd559
nikhil.maurya <nikhil.maurya@nutanix.com>
2025-06-23 15:41:22 +0530

    test

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-// Add response headers for better compatibility
+// Add response headers for better compatibility checking
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
